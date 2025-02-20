7b5bd9a9c906d9e76f11219ccf37c72269dbe05f
Andrew Card <andrewccard04@gmail.com>
2025-02-20 01:17:11 -0800

    started hud, doesnt scale properly with zoom yet

---
+++ scripts/tiles.gd
@@ -0,0 +1,57 @@
+extends ItemList
+
+@onready var camera = $"../.."
+@onready var ui_container = $".."
+@onready var item_list = $"."
+
+
+var tileset = TileSet.new()
+
+func get_tile_texture(coords: Vector2i, source_id: int = 0) -> AtlasTexture:
+	
+	var source = tileset.get_source(source_id) as TileSetAtlasSource
+	var region = source.get_tile_texture_region(coords)
+	
+	var atlas = AtlasTexture.new()
+	atlas.atlas = source.texture
+	atlas.region = region
+	return atlas
+
+func atlas_to_texture2d(atlas_texture: AtlasTexture) -> ImageTexture:
+	# Create an Image from the atlas region
+	var image = Image.new()
+	var region = atlas_texture.region
+	image = atlas_texture.atlas.get_image().get_region(region)
+	
+	# Convert the Image to ImageTexture
+	var texture = ImageTexture.create_from_image(image)
+	return texture
+
+func _ready():
+	
+	var atlas_source = TileSetAtlasSource.new()
+	var texture = preload("res://assets/TileMap.png")
+	atlas_source.texture = texture
+	atlas_source.texture_region_size = Vector2i(16,16)
+	atlas_source.create_tile(Vector2i(2,0))
+	atlas_source.create_tile(Vector2i(3,0))
+	tileset.add_source(atlas_source,0)
+	
+	item_list.add_icon_item(atlas_to_texture2d(get_tile_texture(Vector2i(2,0),0)))
+	item_list.add_icon_item(atlas_to_texture2d(get_tile_texture(Vector2i(3,0),0)))
+	
+	pass
+	
+func _process(delta):
+	
+	var standard_container_size = Vector2(800,144)
+	
+	print("Camera Zoom:" + str(camera.zoom))
+	print("Container Size" + str(ui_container.size))
+	
+	ui_container.scale = Vector2(4,4) / camera.zoom
+	
+	#item_list.icon_scale = 6 / camera.zoom.x
+	#ui_container.set_size(standard_container_size/camera.zoom)
+	
+	pass
